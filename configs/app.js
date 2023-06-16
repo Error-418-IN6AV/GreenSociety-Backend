ce470cf1fba2dbf833d58971d002916667ba89a6
gpinzon-2021163 <gpinzon-2021163@kinal.edu.gt>
2023-06-15 23:48:28 -0600

    Se agrego crud de usuarios

--- configs/app.js
+++ configs/app.js
@@ -5,13 +5,16 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3500;
-
+const port = process.env.PORT || 3000;
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
+
+const userRoutes = require('../src/user/user.routes');
+
+app.use('/user',userRoutes)
 
 exports.initServer = ()=>{
     app.listen(port);
---
+++ src/user/user.model.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    surname: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: Number,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true,
+        uppercase: true
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
