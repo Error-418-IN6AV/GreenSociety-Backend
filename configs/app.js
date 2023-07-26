991690e2dcb82ba587e24ad765be4d9de8dae846
Ecucul-2021681 <ecucul-2021681@kinal.edu.gt>
2023-07-26 17:43:26 -0600

    Revert "Se Agrego arreglo crud y se agrego search"
    This reverts commit 972f150125e6829e2ccf336f0378c9479e10541b.

--- configs/app.js
+++ configs/app.js
@@ -6,15 +6,12 @@ const helmet = require('helmet');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3500;
-const categoryRoutes = require('../src/category/category.routes')
-const productRoutes = require('../src/product/product.routes')
+
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
-app.use('/category', categoryRoutes);
-app.use('/product', productRoutes);
 
 exports.initServer = ()=>{
     app.listen(port);
