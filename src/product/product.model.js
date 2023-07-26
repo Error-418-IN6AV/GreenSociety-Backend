972f150125e6829e2ccf336f0378c9479e10541b
Ecucul-2021681 <ecucul-2021681@kinal.edu.gt>
2023-07-26 17:43:26 -0600

    Se Agrego arreglo crud y se agrego search

---
+++ src/product/product.model.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    stock:{
+        type: Number,
+        required: true
+    },
+    image:{
+        type: String
+    },
+    category:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Categorie',
+        required: true
+    }
+
+},{
+    versionKey: false
+})
+
+module.exports = mongoose.model('Product', productSchema);
